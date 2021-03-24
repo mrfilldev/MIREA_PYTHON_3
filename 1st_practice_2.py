32ca66ce5677fec90bdf47a8b3e65471049a3fa1
Vladislav Filippov <mr.filldev@gmail.com>
2021-03-24 11:47:08 +0300

    update mthfkr

--- 1st_practice_2.py
+++ 1st_practice_2.py
@@ -1,13 +1,29 @@
 import math
-def f13(x,y):
 
 
+def f12(x):
     if x < 160:
         ans = (x ** 8) - (x ** 7)
     elif 160 <= x < 193:
-        ans = math.tg((x ** 3 - x ** 4 - 75) + 5 * x ** 5 - 77)
+        ans = math.tan((pow(x, 3) - pow(x, 4) - 75)) + (5 * (pow(x, 5))) - 77
     elif 193 <= x < 262:
-        ans = (11 * (x ** 7)) - (21 * (x ** 6))
+        ans = (50 * (pow((math.cos(x) + 70 * pow(x, 6)), 6))) + (abs(x))
     elif x >= 262:
-        ans = (42*((x ** 5 + x * 8 + 55) ** 7) - 71 * (x ** 3))
+
+        a = pow(x, 5)
+        b = pow(x, 8)
+        c = pow(x, 3)
+        #d = pow((a + b + c), 7)
+        #e = 42 * (d + 55)
+       # ans = e - (71 * c)
+        ans = 42 * pow((a + b + 55), 7) - 71 * c
+    return ans
+
+
+print(f12(268))
+print(f12(194))
+print(f12(191))
+print(f12(150))
+print(f12(177))
+
 
--- 1st_practice_4.py
+++ 1st_practice_4.py
@@ -1,10 +1,15 @@
 import math
 
-n = int(input('Vvedite N'))
 
-f_1 = 3
-for i in range(1, n+1):
-    f = (((1 / 13) * (f_1 ** 3)) - ((1 / 38) * (f_1 ** 2)))
-    f_1 = f
+def f14(n):
+    f_2 = 3
+    f_1 = 3
+    f = 0
+    for i in range(1, n + 1):
+        f = (((1 / 13) * (f_2 ** 3)) - ((1 / 38) * (f_1 ** 2)))
+        f_2 = f_1
+        f_1 = f
+    return f
 
-print(f - 1)
+
+#print(f14(3))
